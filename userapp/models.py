d759126e0631bd251ca2131eb27f67ff593127a6
andre344 <gveesst@gmail.com>
2023-01-08 02:02:03 +0700

    модель пользователя админка

---
+++ userapp/models.py
@@ -0,0 +1,57 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.contrib.auth.validators import ASCIIUsernameValidator
+from django.utils.translation import gettext_lazy as _
+
+from django.db import models
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    USERNAME_FIELD = 'username'
+
+    username_validator = ASCIIUsernameValidator()
+
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. ASCII letters and digits only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
+    first_name = models.CharField(_("first name"), max_length=150, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    age = models.PositiveIntegerField(blank=True, null=True)
+    email = models.CharField(
+        _("email address"),
+        max_length=256,
+        unique=True,
+        error_messages={
+            "unique": _("A user with that email address already exists."),
+        },
+    )
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_(
+            "Designates whether the user can log into this admin site."
+        ),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. \
+            Unselect this instead of deleting accounts."
+        ),
+    )
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
